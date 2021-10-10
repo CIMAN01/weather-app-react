cd84f602094584bdb09901167f00acefb24ba130
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2021-10-10 11:44:03 -0700

    Format code

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 
 const Forecast = ({iconUrl, weatherDescription, dayName, temp, forecastTempUnit}) => {
-
     return (
-        
         <article className="weather_forecast_item">
-            
             <h3 className="weather_forecast_day">{dayName}</h3>
-                
                 <img src={iconUrl} alt={weatherDescription} className="weather_forecast_icon" />
-
             <h3 className="weather_forecast_temperature"><span className="value">{temp}</span>&deg; {forecastTempUnit}</h3>
-
         </article>
-
     );
-
 }
 
 export default Forecast;
